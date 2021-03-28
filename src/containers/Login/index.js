9cd6b0d5e93f9db5a0c8cab23576119ed428bbc2
Yahyawi Abdel Fatteh <abdelfatteh.yahyawi@weavelines.com>
2021-03-28 23:01:59 +0100

    login

---
+++ src/containers/Login/index.js
@@ -0,0 +1,57 @@
+import React, { useState, useContext } from "react";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import { TokenContext } from "../../context";
+
+import "./style.scss";
+
+const Login = () => {
+  const [token, setToken] = useContext(TokenContext);
+  const [state, setState] = useState({
+    email: "",
+    passsword: "",
+  });
+
+  const handleChange = (evt) => {
+    const value = evt.target.value;
+    setState({
+      ...state,
+      [evt.target.name]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log("test", state, setToken);
+    if (state.email === "test@test.com" && state.passsword === "1234") {
+      console.log("test");
+      setToken(true);
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="entry-group">
+        <Input
+          label="Email adresse"
+          type="text"
+          className="input"
+          name="email"
+          value={state.tache}
+          onChange={(e) => handleChange(e)}
+        />
+        <Input
+          label="Password"
+          type="text"
+          className="input"
+          name="passsword"
+          value={state.description}
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+
+      <Button theme="primary" content="Soumettre" onClick={handleSubmit} />
+    </div>
+  );
+};
+
+export default Login;
