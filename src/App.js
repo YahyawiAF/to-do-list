a4c52d2cb0225e4edbdf5341ff213290a8415857
Yahyawi Abdel Fatteh <abdelfatteh.yahyawi@weavelines.com>
2021-03-28 23:00:11 +0100

    app provider setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import ListToDo from "./containers/ListTode.js";
+import ListToDo from "./containers/ListTodojs";
+import Login from "./containers/Login";
+import { ToDoContext, TokenContext } from "./context";
+import usePersistedState from "./utils/usePersistedState";
 
 import "./App.scss";
 
 function App() {
+  const [todos, setTodos] = usePersistedState("todos", []);
+  const [token, setToken] = usePersistedState("token", false);
   return (
     <div className="App">
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route path="/list" exact component={ListToDo} />
-        </Switch>
-      </Router>
+      <TokenContext.Provider value={[token, setToken]}>
+        <Router>
+          <NavBar auth={token} />
+          <Switch>
+            <Route path="/" exact component={Login} />
+            <ToDoContext.Provider value={[todos, setTodos]}>
+              <Route path="/list" component={ListToDo} />
+            </ToDoContext.Provider>
+          </Switch>
+        </Router>
+      </TokenContext.Provider>
     </div>
   );
 }
